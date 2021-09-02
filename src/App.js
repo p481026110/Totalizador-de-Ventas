70cfb0e91d96350c31e4c2b8fc58582754a17e4d
tiaraRS <74866417+tiaraRS@users.noreply.github.com>
2021-09-02 09:06:10 -0200

    A_Precio

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 const cantidad = document.querySelector("#cantidad-input");
-
+const precio = document.querySelector("#precio-input")
 const form = document.querySelector("#totalizador-form");
 
 form.addEventListener("submit", event =>{
-    //event.preventDefault();
-    alert("Cantidad ingresada:"+ cantidad.value);
+    event.preventDefault();
+    alert("Cantidad ingresada:"+ cantidad.value, "Precio del item: "+precio);
+
 })
