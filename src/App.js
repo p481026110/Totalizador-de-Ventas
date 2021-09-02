ef91de9c6b470812384eb9b90c471fe6f683301c
tiaraRS <74866417+tiaraRS@users.noreply.github.com>
2021-09-02 09:39:12 -0200

    EstadoManual

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 const cantidad = document.querySelector("#cantidad-input");
 const precio = document.querySelector("#precio-input")
+const estado = document.querySelector("#estado-input")
 const form = document.querySelector("#totalizador-form");
+
 var impuesto = 0.0825; //california
 
 form.addEventListener("submit", event =>{
     event.preventDefault();
-    alert("Cantidad ingresada: "+ cantidad.value+ "\nPrecio del item: "+precio.value+"\n Impuesto(predeterminado): "+impuesto*100+"%");
+    alert("Cantidad ingresada: "+ cantidad.value+ 
+    "\nPrecio del item: "+precio.value+
+    "\n Impuesto(predeterminado): "+impuesto*100+"%"+
+    "\n Estado ingresado: "+estado.value);
     var precioTotal = cantidad.value * precio.value;
     var precioTotalConImpuesto = precioTotal + impuesto*precioTotal; 
     alert("Precio total: "+precioTotal+"\nPrecio total con impuesto: "+precioTotalConImpuesto);
