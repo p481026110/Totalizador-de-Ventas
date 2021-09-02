85f2b032e45dca3ace4112b7879d703ef40b8fc1
tiaraRS <74866417+tiaraRS@users.noreply.github.com>
2021-09-02 08:55:59 -0200

    1 Visualizar cantidad

--- src/App.js
+++ src/App.js
@@ -1,1 +1,8 @@
-alert("Presiona OK si estas list@ para empezar con javascript")
+const cantidad = document.querySelector("#cantidad-input");
+
+const form = document.querySelector("#totalizador-form");
+
+form.addEventListener("submit", event =>{
+    //event.preventDefault();
+    alert("Cantidad ingresada:"+ cantidad.value);
+})
