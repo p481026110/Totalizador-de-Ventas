c86f3ecbf89126f6431d6e1f0a96c88492d87232
tiaraRS <74866417+tiaraRS@users.noreply.github.com>
2021-09-05 15:29:00 -0200

    Precio con impuesto de estado seleccionado

--- src/App.js
+++ src/App.js
@@ -5,12 +5,34 @@ const form = document.querySelector("#totalizador-form");
 
 var impuesto = 0.0825; //california
 
+function impuestoPorEstado(){
+    var impuestoEstado = impuesto;
+    switch (estado.value){
+        case "UT":
+            impuestoEstado = 0.0665;
+            break;
+        case "NV":
+            impuestoEstado = 0.08;
+            break;
+        case "TX":
+            impuestoEstado = 0.0625;
+            break;
+        case "AL":
+            impuestoEstado = 0.04;
+            break;
+        default :
+            break;
+    }
+    return impuestoEstado;
+}
+
 form.addEventListener("submit", event =>{
     event.preventDefault();
+    impuesto = impuestoPorEstado();
     alert("Cantidad ingresada: "+ cantidad.value+ 
     "\nPrecio del item: "+precio.value+
-    "\n Impuesto(predeterminado): "+impuesto*100+"%"+
-    "\n Estado ingresado: "+Impuesto.value);
+    "\n Impuesto: "+impuesto*100+"%"+
+    "\n Estado ingresado: "+estado.value);
     var precioTotal = cantidad.value * precio.value;
     var precioTotalConImpuesto = precioTotal + impuesto*precioTotal; 
     alert("Precio total: "+precioTotal+"\nPrecio total con impuesto: "+precioTotalConImpuesto);
